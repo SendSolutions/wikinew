1b99351f2c46ad0fac6088d5622d605b2e330b8a
Erwise Development <raphael.goncalves@erwise.com.br>
2025-03-01 21:42:55 -0300

    first commit

---
+++ lang/pt/activities.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
+return [
+
+    // Pages
+    'page_create'                 => 'criou a página',
+    'page_create_notification'    => 'Página criada com sucesso',
+    'page_update'                 => 'página atualizada',
+    'page_update_notification'    => 'Página atualizada com sucesso.',
+    'page_delete'                 => 'página eliminada',
+    'page_delete_notification'    => 'Página excluída com sucesso.',
+    'page_restore'                => 'página restaurada',
+    'page_restore_notification'   => 'Página restaurada com sucesso',
+    'page_move'                   => 'página movida',
+    'page_move_notification'      => 'Página movida com sucesso',
+
+    // Chapters
+    'chapter_create'              => 'Release criado',
+    'chapter_create_notification' => 'Release criado com sucesso',
+    'chapter_update'              => 'Release atualizado',
+    'chapter_update_notification' => 'Release atualizado com sucesso',
+    'chapter_delete'              => 'Release excluído',
+    'chapter_delete_notification' => 'Release excluído com sucesso',
+    'chapter_move'                => 'Release movido',
+    'chapter_move_notification' => 'Release movido com sucesso',
+
+    // Books
+    'book_create'                 => 'Módulo criado',
+    'book_create_notification'    => 'Módulo criado com sucesso',
+    'book_create_from_chapter'              => 'Release convertido para lívro',
+    'book_create_from_chapter_notification' => 'Release convertido em Módulo com sucesso',
+    'book_update'                 => 'Módulo atualizado',
+    'book_update_notification'    => 'Módulo atualizado com sucesso',
+    'book_delete'                 => 'Módulo eliminado',
+    'book_delete_notification'    => 'Módulo eliminado com sucesso',
+    'book_sort'                   => 'Módulo ordenado',
+    'book_sort_notification'      => 'Módulo reordenado com sucesso',
+
+    // Bookshelves
+    'bookshelf_create'            => 'Sistema criada',
+    'bookshelf_create_notification'    => 'Sistema criada com sucesso',
+    'bookshelf_create_from_book'    => 'Módulo convertido para Sistema',
+    'bookshelf_create_from_book_notification'    => 'Módulo convertido em prateleira com sucesso',
+    'bookshelf_update'                 => 'Sistema atualizada',
+    'bookshelf_update_notification'    => 'Sistema atualizada com sucesso',
+    'bookshelf_delete'                 => 'prateleira excluída',
+    'bookshelf_delete_notification'    => 'Sistema eliminada com sucesso',
+
+    // Revisions
+    'revision_restore' => 'revisão restaurada',
+    'revision_delete' => 'revisão eliminada',
+    'revision_delete_notification' => 'Revisão eliminada com sucesso',
+
+    // Favourites
+    'favourite_add_notification' => '":name" foi adicionado aos seus favoritos',
+    'favourite_remove_notification' => '":name" foi removido dos seus favoritos',
+
+    // Watching
+    'watch_update_level_notification' => 'Ver preferências atualizadas com sucesso',
+
+    // Auth
+    'auth_login' => 'sessão iniciada',
+    'auth_register' => 'registado como novo utilizador',
+    'auth_password_reset_request' => 'pedido a redefinição da palavra-passe',
+    'auth_password_reset_update' => 'redifinir palavra-passe do utilizador',
+    'mfa_setup_method' => 'configurar método de duplo fator',
+    'mfa_setup_method_notification' => 'Método de autenticação por múltiplos-fatores configurado com sucesso',
+    'mfa_remove_method' => 'método de duplo fator removido',
+    'mfa_remove_method_notification' => 'Método de autenticação por múltiplos-fatores removido com sucesso',
+
+    // Settings
+    'settings_update' => 'configurações atualizadas',
+    'settings_update_notification' => 'Configurações atualizadas com sucesso',
+    'maintenance_action_run' => 'ação de manutenção executada',
+
+    // Webhooks
+    'webhook_create' => 'webhook criado',
+    'webhook_create_notification' => 'Webhook criado com sucesso',
+    'webhook_update' => 'atualizar um webhook',
+    'webhook_update_notification' => 'Webhook criado com sucesso',
+    'webhook_delete' => 'eliminar webhook',
+    'webhook_delete_notification' => 'Webhook criado com sucesso',
+
+    // Imports
+    'import_create' => 'created import',
+    'import_create_notification' => 'Import successfully uploaded',
+    'import_run' => 'updated import',
+    'import_run_notification' => 'Content successfully imported',
+    'import_delete' => 'deleted import',
+    'import_delete_notification' => 'Import successfully deleted',
+
+    // Users
+    'user_create' => 'utilizador criado',
+    'user_create_notification' => 'Utilizador criado com sucesso',
+    'user_update' => 'utilizador atualizado',
+    'user_update_notification' => 'Utilizador atualizado com sucesso',
+    'user_delete' => 'utilizador eliminado',
+    'user_delete_notification' => 'Utilizador removido com sucesso',
+
+    // API Tokens
+    'api_token_create' => 'created API token',
+    'api_token_create_notification' => 'API token criado com sucesso',
+    'api_token_update' => 'updated API token',
+    'api_token_update_notification' => 'API token atualizado com sucesso',
+    'api_token_delete' => 'deleted API token',
+    'api_token_delete_notification' => 'API token atualizado com sucesso',
+
+    // Roles
+    'role_create' => 'cargo criado',
+    'role_create_notification' => 'Cargo criado com sucesso',
+    'role_update' => 'cargo atualizado',
+    'role_update_notification' => 'Cargo atualizado com sucesso',
+    'role_delete' => 'cargo eliminado',
+    'role_delete_notification' => 'Cargo excluído com sucesso',
+
+    // Recycle Bin
+    'recycle_bin_empty' => 'reciclagem vazia',
+    'recycle_bin_restore' => 'restaurado da reciclagem',
+    'recycle_bin_destroy' => 'removido da reciclagem',
+
+    // Comments
+    'commented_on'                => 'comentado a',
+    'comment_create'              => 'comentário adicionado',
+    'comment_update'              => 'comentário atualizado',
+    'comment_delete'              => 'comentário eliminado',
+
+    // Sort Rules
+    'sort_rule_create' => 'created sort rule',
+    'sort_rule_create_notification' => 'Sort rule successfully created',
+    'sort_rule_update' => 'updated sort rule',
+    'sort_rule_update_notification' => 'Sort rule successfully updated',
+    'sort_rule_delete' => 'deleted sort rule',
+    'sort_rule_delete_notification' => 'Sort rule successfully deleted',
+
+    // Other
+    'permissions_update'          => 'permissões atualizadas',
+];
---
+++ resources/views/home/parts/sidebar.blade.php
@@ -0,0 +1,21 @@
+@if(count($draftPages) > 0)
+    <div id="recent-drafts" class="mb-xl">
+        <h5>{{ trans('entities.my_recent_drafts') }}</h5>
+        @include('entities.list', ['entities' => $draftPages, 'style' => 'compact'])
+    </div>
+@endif
+
+@if(count($favourites) > 0)
+    <div id="top-favourites" class="mb-xl">
+        <h5>{{ trans('entities.my_most_viewed_favourites') }}</h5>
+        @include('entities.list', [
+            'entities' => $favourites,
+            'style' => 'compact',
+        ])
+        <a href="{{ url('/favourites')  }}" class="text-muted block py-xs">{{ trans('common.view_all') }}</a>
+    </div>
+@endif
+
+
+
+
